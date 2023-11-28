98580452bc7ce4b3d7ca55a10fb2309b05e91fc3
BranBayou <berhan.baye@gmail.com>
2023-11-28 04:48:38 +0300

    Add api endpoing

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,16 @@
-// Entry point for the build script in your package.json
-import "@hotwired/turbo-rails"
-import "./controllers"
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function App() {
+  return (
+  <dib>
+    <h1>Hello World!</h1>
+    <p>This is react-rails project</p>
+  </dib>
+  );
+}
+
+ReactDOM.render(
+  <App/>,
+  document.getElementById('root'),
+);
